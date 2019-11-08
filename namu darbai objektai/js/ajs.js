df23c16843adc53dc13bd035ac127c03f7513e58
jon4st <45789952+jon4st@users.noreply.github.com>
2019-11-08 15:24:42 +0200

    namu darbai su objektais

---
+++ namu darbai objektai/js/ajs.js
@@ -0,0 +1,82 @@
+let knygos = [
+grozine:[
+{
+			ISBN:1234,
+			Leidimo metai:1983,
+			Pavadinimas:'trys parsiukai',
+			Puslapių skaičius:235
+},
+{
+			ISBN:15489,
+			Leidimo metai:1983,
+			Pavadinimas:'rozes',
+			Puslapių skaičius:25
+},
+{
+			ISBN:1673,
+			Leidimo metai:2019,
+			Pavadinimas:'viskas bus gerai',
+			Puslapių skaičius:763
+}
+],
+moksline:[
+{
+			ISBN:56746,
+			Leidimo metai:2011,
+			Pavadinimas:'angelai',
+			Puslapių skaičius:167
+},
+{
+			ISBN:4679,
+			Leidimo metai:1983,
+			Pavadinimas:'myliu js',
+			Puslapių skaičius:607
+},
+{
+			ISBN:4444,
+			Leidimo metai:2004,
+			Pavadinimas:'pamokos',
+			Puslapių skaičius:123
+}
+],
+fantastika:[
+{
+			ISBN:14697,
+			Leidimo metai:2003,
+			Pavadinimas:'ana ir karalius',
+			Puslapių skaičius:631
+},
+{
+			ISBN:15489,
+			Leidimo metai:1983,
+			Pavadinimas:'parsiukai',
+			Puslapių skaičius:235
+},
+{
+			ISBN:364,
+			Leidimo metai:2010,
+			Pavadinimas:'gerai',
+			Puslapių skaičius:89
+},
+{
+			ISBN:8976,
+			Leidimo metai:2012,
+			Pavadinimas:'tuoj tuoj',
+			Puslapių skaičius:648
+},
+{
+			ISBN:62176,
+			Leidimo metai:2000,
+			Pavadinimas:'marsieciai',
+			Puslapių skaičius:469
+},
+{
+			ISBN:267982,
+			Leidimo metai:1995,
+			Pavadinimas:'nuostabioji karta',
+			Puslapių skaičius:359
+}
+
+]
+
+]
